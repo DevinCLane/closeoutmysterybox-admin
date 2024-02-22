92ca6e014500510d9737ec08b4b21e2fb53c9a72
Devin Lane <devin.c.lane@gmail.com>
2024-02-21 17:21:03 -0800

    setup auth for dashboard

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,36 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+    children,
+    params,
+}: {
+    children: React.ReactNode;
+    params: { storeId: string };
+}) {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect("/sign-in");
+    }
+
+    // load the store with the id passed from the redirect from the root folder
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId,
+        },
+    });
+
+    if (!store) {
+        redirect("/");
+    }
+
+    return (
+        <>
+            <div>this will be a navbar</div>
+            {children}
+        </>
+    );
+}
