a1a5ab8e7884d684316cfc662841a4aacf55fba8
Devin Lane <devin.c.lane@gmail.com>
2024-02-21 17:01:48 -0800

    toast tab store modal

---
+++ app/api/stores/route.ts
@@ -0,0 +1,36 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+// API for creating a store
+
+export async function POST(req: Request) {
+    try {
+        // currently logged in user
+        const { userId } = auth();
+        const body = await req.json();
+
+        const { name } = body;
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if (!name) {
+            return new NextResponse("Name is required", { status: 400 });
+        }
+
+        const store = await prismadb.store.create({
+            data: {
+                name,
+                userId,
+            },
+        });
+
+        return NextResponse.json(store);
+    } catch (error) {
+        console.log("[STORES_POST]", error);
+        return new NextResponse("Internal error", { status: 500 });
+    }
+}
