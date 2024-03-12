81996986fc51b8903264320ba71fc2feeb49ba83
Devin Lane <devin.c.lane@gmail.com>
2024-03-11 18:21:37 -0700

    update cell actions for categories

--- app/(dashboard)/[storeId]/(routes)/categories/components/cell-actions.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-actions.tsx
@@ -16,10 +16,10 @@ import {
 import { Button } from "@/components/ui/button";
 import { AlertModal } from "@/components/modals/alert-modal";
 
-import { BillboardColumn } from "./columns";
+import { CategoryColumn } from "./columns";
 
 interface CellActionProps {
-    data: BillboardColumn;
+    data: CategoryColumn;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -31,18 +31,18 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
     const onCopy = (id: string) => {
         navigator.clipboard.writeText(id);
-        toast.success("Billboard ID copied to clipboard.");
+        toast.success("Category ID copied to clipboard.");
     };
 
     const onDelete = async () => {
         try {
             setLoading(true);
-            await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+            await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
             router.refresh();
-            toast.success("Billboard deleted.");
+            toast.success("Category deleted.");
         } catch (error) {
             toast.error(
-                "Make sure you remove all categories using this billboard first."
+                "Make sure you remove all products using this category first."
             );
         } finally {
             setLoading(false);
@@ -74,7 +74,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
                     <DropdownMenuItem
                         onClick={() =>
                             router.push(
-                                `/${params.storeId}/billboards/${data.id}`
+                                `/${params.storeId}/categories/${data.id}`
                             )
                         }
                     >
